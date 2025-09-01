fc74c803e413f6958e64192d73c23bb7db70ca54
Caleb <baseline7@live.com>
2025-09-01 16:30:20 -0700

    Remove import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 //import App from './App.jsx';
 import Scene from './three/Scene.jsx';
-import ScrollHome from './three/ScrollHome.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
